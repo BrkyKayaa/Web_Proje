cb96edcea9fccebd29b5e99e69a817739d2b99fe
slhkrkya <slhkrkyh@gmail.com>
2025-04-16 15:51:45 +0300

    deneme

--- src/main/java/com/proje/fitnesapp/FitnesappApplication.java
+++ src/main/java/com/proje/fitnesapp/FitnesappApplication.java
@@ -8,7 +8,6 @@ public class FitnesappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnesappApplication.class, args);
-		System.out.println("Hello Salih!");
 	}
 
 }
